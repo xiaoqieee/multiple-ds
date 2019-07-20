826527ff28bb866141ee154c7a2502de257c9fd7
fengqi3389 <fengqi3389@sina.com>
2019-07-20 21:22:17 +0800

    初步完成提交-版本2-优化spring配置文件

--- src/main/java/com/hawcore/framework/multiplyds/MultiplyDsApplication.java
+++ src/main/java/com/hawcore/framework/multiplyds/MultiplyDsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:spring/spring-context.xml")
 public class MultiplyDsApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hawcore/framework/multiplyds/config/MultipleDSAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.hawcore.framework.multiplyds.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:spring/spring-context.xml")
+public class MultipleDSAutoConfiguration {
+
+}
