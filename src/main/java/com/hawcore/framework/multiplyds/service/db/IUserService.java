29adaca035e903050ef4e3f87b617fcf61fe1336
fengqi3389 <fengqi3389@sina.com>
2019-07-21 21:13:35 +0800

    添加代码生成代码

--- src/main/java/com/hawcore/framework/multiplyds/service/db/IUserService.java
+++ src/main/java/com/hawcore/framework/multiplyds/service/db/IUserService.java
@@ -3,9 +3,6 @@ package com.hawcore.framework.multiplyds.service.db;
 import com.baomidou.mybatisplus.service.IService;
 import com.hawcore.framework.multiplyds.entity.db.User;
 
-import java.io.Serializable;
-import java.util.List;
-
 /**
  * <p>
  *  服务类
